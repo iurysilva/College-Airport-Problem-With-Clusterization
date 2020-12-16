557125a8506310bf6fe5344e02a2eb358e877f3c
Guilherme H <39814358+guitani@users.noreply.github.com>
2020-12-16 17:29:45 -0300

    Bairros do centroide

--- Objetos/centroide.py
+++ Objetos/centroide.py
@@ -5,6 +5,7 @@ class Centroide(object):
     def __init__(self, nome):
         self.nome = nome
         self.posicao = [0,0]
+        self.bairros = []
 
     def gera_posicao_aleatoria(self, bd):
         posicao_aleatoria = np.random.randint(0, bd.numero_bairros)
