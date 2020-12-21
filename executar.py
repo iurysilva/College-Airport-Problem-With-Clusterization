de41fcfa2f5a9491a9d85d6398463dfdf41e26ea
Guilherme H <39814358+guitani@users.noreply.github.com>
2020-12-21 12:28:35 -0300

    Adição de teste de plotagem

---
+++ executar.py
@@ -0,0 +1,42 @@
+from Objetos import Kmeans, Centroide, BancoDeDados
+import matplotlib.pyplot as plt
+
+teste = Kmeans(3)
+banco = BancoDeDados()
+
+x0 = []
+y0 = []
+
+x1 = []
+y1 = []
+
+x2 = []
+y2 = []
+
+xcentroide = []
+ycentroide = []
+bairros = list(banco.bairros)
+
+for i in range (3):
+    teste.centroides[i].gera_posicao_aleatoria(banco)
+    xcentroide.append(teste.centroides[i].posicao[0])
+    ycentroide.append(teste.centroides[i].posicao[1])
+
+teste.clusterizacao(banco)
+print(teste.centroides[0].bairros)
+print(teste.centroides[1].bairros)
+print(teste.centroides[2].bairros)
+
+for bairro in banco.bairros:
+    if banco.bairros[bairro]['centroide'] == 0:
+        x0.append(banco.bairros[bairro]['coordenadas'][0])
+        y0.append(banco.bairros[bairro]['coordenadas'][1])
+    elif banco.bairros[bairro]['centroide'] == 1:
+        x1.append(banco.bairros[bairro]['coordenadas'][0])
+        y1.append(banco.bairros[bairro]['coordenadas'][1])
+    elif banco.bairros[bairro]['centroide'] == 2:
+        x2.append(banco.bairros[bairro]['coordenadas'][0])
+        y2.append(banco.bairros[bairro]['coordenadas'][1])
+
+plt.plot(xcentroide[0], ycentroide[0], 'go', xcentroide[1], ycentroide[1], 'bo', xcentroide[2], ycentroide[2], 'yo', x0, y0,'g^', x1, y1, 'b^',x2, y2, 'y^')
+plt.show()
