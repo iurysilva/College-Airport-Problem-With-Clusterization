b9aab8bb360a782488909d8a71983ff05183a516
Marco Aurélio Nascimento Jr <marconasc505@gmail.com>
2020-12-25 21:06:36 -0300

    Update executar.py
    Só pra conseguir executar pelo terminal mudando os parâmetros

--- executar.py
+++ executar.py
@@ -3,12 +3,6 @@ from Animacoes import rodar_animacao_2d
 import sys
 import copy
 
-movimento_minimo = 0.0000001
-execucoes = 20
-iteracoes = 1000
-animacao = False
-velocidade_animacao = 500  # milissegundos
-clusters = 4
 
 resultados = []
 banco = BancoDeDados()
@@ -26,27 +20,31 @@ def iterar(kmeans):
     return kmeans
 
 
-if animacao:
-    kmeans = Kmeans(clusters, iteracoes, movimento_minimo, banco)
-    kmeans.gera_posicoes_aleatorias()
-    rodar_animacao_2d(kmeans, velocidade_animacao)
-else:
-    menor_distancia_total = sys.maxsize
-    melhor_execucao = 0
-    for execucao in range(execucoes):
+def executar_k_means(clusters=4, movimento_minimo=0.0000001, execucoes=20, iteracoes=1000, animacao=False, velocidade_animacao=500):
+    if animacao:
         kmeans = Kmeans(clusters, iteracoes, movimento_minimo, banco)
         kmeans.gera_posicoes_aleatorias()
-        resultado = copy.deepcopy(iterar(kmeans))
-        resultados.append(resultado)
-        if resultado.distancia_total < menor_distancia_total:
-            menor_distancia_total = resultado.distancia_total
-            melhor_execucao = execucao
+        rodar_animacao_2d(kmeans, velocidade_animacao)
+    else:
+        menor_distancia_total = sys.maxsize
+        melhor_execucao = 0
+        for execucao in range(execucoes):
+            kmeans = Kmeans(clusters, iteracoes, movimento_minimo, banco)
+            kmeans.gera_posicoes_aleatorias()
+            resultado = copy.deepcopy(iterar(kmeans))
+            resultados.append(resultado)
+            if resultado.distancia_total < menor_distancia_total:
+                menor_distancia_total = resultado.distancia_total
+                melhor_execucao = execucao
 
-    for resultado in range(execucoes):
-        print("Resultado %d" % resultado)
-        print("Iteracoes necessárias: %d" % resultados[resultado].iteracoes_feitas)
-        print("Distância total: %f \n" % resultados[resultado].distancia_total)
+        for resultado in range(execucoes):
+            print("Resultado %d" % (resultado+1))
+            print("Iteracoes necessárias: %d" % resultados[resultado].iteracoes_feitas)
+            print("Distância total: %f \n" % resultados[resultado].distancia_total)
 
-    print("melhor distancia: %f" % menor_distancia_total)
-    print("execucao: %d" % melhor_execucao)
-    resultados[melhor_execucao].plotar_mapa()
+        print("Melhor distância: %f" % menor_distancia_total)
+        print("Na execução: %d" % melhor_execucao)
+        resultados[melhor_execucao].plotar_mapa()
+
+if __name__ == '__main__':
+    executar_k_means()
